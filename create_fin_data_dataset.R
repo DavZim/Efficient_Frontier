7f23c99960e9aa7b32a080db73a57d0706820ac3
DavZim <david.zimmermann@zu.de>
2016-05-18 15:10:10 +0200

    added dat

--- create_fin_data_dataset.R
+++ create_fin_data_dataset.R
@@ -6,23 +6,23 @@ lag <- function(x) c(NA, x[1:(length(x) - 1)])
 getData <- function(tickers, long = T) {
   # iterate through the tickers and get the last adjusted price in a data.table
   res <- lapply(tickers, function(x) {
-    
+
     dat <- getSymbols(x, from = "2000-01-01", auto.assign = F)
-    
-    dt <- data.table(date = as.Date(index(dat)), 
+
+    dt <- data.table(date = as.Date(index(dat)),
                      ticker = x,
                      price = as.numeric(Ad(dat)))
     return(dt)
   })
-  
+
   # combine the list to one data.table
   res <- rbindlist(res)
-  
+
   # cast the data if the user wants to get the data in a wide format
   if (!long) {
     res <- dcast(res, date ~ ticker)
   }
-  
+
   return(res)
 }
 
@@ -36,4 +36,4 @@ dt[, ':=' (mt_price = mean(price),
 dt <- dt[, .(date = mt_date, ticker, price = mt_price)]
 dt <- unique(dt)
 
-write.csv(dt, file = "data_eff_front.csv", row.names = F)
+write.csv(dt, file = "fin_data.csv", row.names = F)
---
+++ create_multiple_asset_dataset.R
@@ -0,0 +1,16 @@
+# load the functions, libraries etc
+source("R/functions.R")
+
+set.seed(12345)
+df <- data.table(x = rnorm(10000, mean = 0.07, sd = 0.05))
+
+y_mean <- 0.03
+y_sd <- 0.02
+
+z_mean <- 0.04
+z_sd <- 0.03
+
+df[, y := rmultvar(x, r = 0, y_mean, y_sd)]
+df[, z := rmultvar(x, r = 0, z_mean, z_sd)]
+
+write.csv(df, file = "../mult_assets.csv")
